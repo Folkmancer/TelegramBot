917d696db58935ee92fcd4ce1d59edc41ef9db75
folkmancer <folkmancer@gmail.com>
2019-08-01 17:13:38 +0500

    fix dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-docker login -u $DOCKER_USER --password-stdin= $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t folkmancer-telegrambot:$TAG .
-docker push folkmancer-telegrambot
+#docker build -f TinyTinaBot/Dockerfile -t folkmancer-telegrambot:$TAG .
+#docker push folkmancer-telegrambot
+
+docker build -f TinyTinaBot/Dockerfile .
+docker push tinytinabot
