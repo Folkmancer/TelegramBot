16e1f58660edea7ccbc1bbe68613992ed46e9540
Folkmancer <folkmancer@gmail.com>
2019-08-02 12:00:16 +0500

    fix image name

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -3,7 +3,7 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 #heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 #heroku container:push web --app $HEROKU_APP_NAME
-docker tag aspnetapp registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag folkmancer/tinytinabot registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web                
 #curl https://cli-assets.heroku.com/install.sh | sh
 heroku container:release web -a $HEROKU_APP_NAME
