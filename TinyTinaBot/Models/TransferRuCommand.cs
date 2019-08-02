45ac8754c6f958c7eb9702e03a9f6f02e673697b
folkmancer <folkmancer@gmail.com>
2019-08-03 00:50:21 +0500

    Added class for ru lang transfer

---
+++ TinyTinaBot/Models/TransferRuCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TinyTinaBot.Models
+{
+    public class TransferRuCommand : ICommand
+    {
+        public string Name => @"/ru";
+
+        public bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId, "TEST RU", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
