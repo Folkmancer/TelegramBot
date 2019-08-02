43b38e53e3281b89058c855860467c108ab530e1
folkmancer <folkmancer@gmail.com>
2019-08-03 00:52:33 +0500

    Added class for en lang transfer

---
+++ TinyTinaBot/Models/TransferEnCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TinyTinaBot.Models
+{
+    public class TransferEnCommand : ICommand
+    {
+        public string Name => @"/en";
+
+        public bool Contains(Message message)
+        {
+            if (message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return false;
+
+            return message.Text.Contains(this.Name);
+        }
+
+        public async Task Execute(Message message, TelegramBotClient botClient)
+        {
+            var chatId = message.Chat.Id;
+            await botClient.SendTextMessageAsync(chatId, "TEST EN", parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+        }
+    }
+}
