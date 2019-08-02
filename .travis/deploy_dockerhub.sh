211d6723f6311f16a9ab7b33d9b38366ba07fd5c
Folkmancer <folkmancer@gmail.com>
2019-08-02 12:29:23 +0500

    Fixed secure

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+#docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f TinyTinaBot/Dockerfile -t folkmancer/tinytinabot:latest .
-docker push folkmancer/tinytinabot:latest
+docker build -f TinyTinaBot/Dockerfile -t $DOCKER_IMAGE_NAME:$TAG .
+docker push $DOCKER_IMAGE_NAME:$TAG
--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 #heroku plugins:install heroku-container-registry
-docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
+echo "$HEROKU_API_KEY" | docker login -u _ --password-stdin registry.heroku.com
+#docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 #heroku container:push web --app $HEROKU_APP_NAME
-docker tag folkmancer/tinytinabot registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag $DOCKER_IMAGE_NAME registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web                
 curl https://cli-assets.heroku.com/install.sh | sh
 heroku container:release web -a $HEROKU_APP_NAME
