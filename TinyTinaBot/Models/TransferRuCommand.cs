e43bca9f002ca1bd9fc9e54f9a966c551acf0afc
folkmancer <folkmancer@gmail.com>
2019-08-04 01:01:34 +0500

    edit ru translator

--- TinyTinaBot/Models/TransferRuCommand.cs
+++ TinyTinaBot/Models/TransferRuCommand.cs
@@ -19,7 +19,18 @@ namespace TinyTinaBot.Models
         public async Task Execute(Message message, TelegramBotClient botClient)
         {
             var chatId = message.Chat.Id;
-            await botClient.SendTextMessageAsync(chatId, TextLayoutTranslator.TranslateIntoRU(message.Text), parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+            await botClient.SendChatActionAsync(chatId, Telegram.Bot.Types.Enums.ChatAction.Typing);
+            await botClient.SendTextMessageAsync(chatId, "Дайте мне текст!");
+            botClient.StartReceiving();
+            if (botClient.IsReceiving)
+            {
+                await botClient.SendTextMessageAsync(chatId, TextLayoutTranslator.TranslateIntoRU(message.Text), parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+                botClient.StopReceiving();
+                // if (!IsValidUsername(update.Message.ReplyToMessage.Text)) return;
+                //SaveUsernameToDb(update.Message.Chat.Id, update.Message.ReplyToMessage.Text);
+                //Bot.SendChatAction(update.Message.Chat.Id, ChatAction.Typing);
+                //Bot.SendTextMessage(update.Message.Chat.Id, "Username has been successfully saved!");
+            }       
         }
     }
 }
