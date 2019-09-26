e175c0d7c6afd1a28fb32c4e576271189cb4dc0f
Folkmancer <folkmancer@gmail.com>
2019-09-26 11:27:58 +0300

    Edited Startup

--- TinyTinaBot/Startup.cs
+++ TinyTinaBot/Startup.cs
@@ -20,7 +20,7 @@ namespace TinyTinaBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllers().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
