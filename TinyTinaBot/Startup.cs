895f87333cdaa157d364117522a1f25dd4203c11
Folkmancer <folkmancer@gmail.com>
2019-09-26 10:34:27 +0300

    Fix

--- TinyTinaBot/Startup.cs
+++ TinyTinaBot/Startup.cs
@@ -30,11 +30,11 @@ namespace TinyTinaBot
             {
                 app.UseDeveloperExceptionPage();
             }
-            //else
-            //{
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
