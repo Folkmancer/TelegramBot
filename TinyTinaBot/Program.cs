b818963169151bc727e2ff071255844019abf442
Folkmancer <folkmancer@gmail.com>
2019-09-26 13:34:22 +0300

    Second try migrate from ASP.NET Core 2.2 to 3.0

--- TinyTinaBot/Program.cs
+++ TinyTinaBot/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using TinyTinaBot.Models;
@@ -18,11 +17,14 @@ namespace TinyTinaBot
             BotSettings.Name = config["BotName"];
             BotSettings.Key = config["BotKey"];
 
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
