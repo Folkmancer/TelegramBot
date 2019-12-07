1c8dedef5b9c246a9763914cf5470864d243faa6
Folkmancer <folkmancer@gmail.com>
2019-12-07 17:26:30 +0300

    Migrate from ASP.NET Core 3.0 to 3.1, updated nuget packages

--- TinyTinaBot/Startup.cs
+++ TinyTinaBot/Startup.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using TinyTinaBot.Models;
 
 namespace TinyTinaBot
@@ -19,13 +16,11 @@ namespace TinyTinaBot
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
