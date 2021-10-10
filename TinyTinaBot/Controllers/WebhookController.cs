0fd63833f91c1f28e21fbefc1bd79df867f54315
Folkmancer <folkmancer@gmail.com>
2021-10-10 16:52:36 +0300

    Added new controller for webhook.

---
+++ TinyTinaBot/Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TinyTinaBot.Services;
+
+namespace TinyTinaBot.Controllers
+{
+    public class WebhookController : ControllerBase
+    {
+        private readonly ILogger<WebhookController> logger;
+        private readonly IUpdateHandlerService updateHandlerService;
+
+        public WebhookController(ILogger<WebhookController> logger, IUpdateHandlerService updateHandlerService)
+        {
+            this.logger = logger;
+            this.updateHandlerService = updateHandlerService;
+        }
+
+        public async Task<IActionResult> Post(Update update)
+        {
+            logger.LogInformation($"Received post message");
+            await updateHandlerService.EchoAsync(update);
+            return Ok();
+        }
+    }
+}
