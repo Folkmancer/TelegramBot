067cd9c9b76a4c4c526c36e03eedbd86d25ef28d
Folkmancer <folkmancer@gmail.com>
2021-10-10 16:56:28 +0300

    Configured startup.Removed unnecessary code from program class.

--- TinyTinaBot/Program.cs
+++ TinyTinaBot/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using TinyTinaBot.Models;
 
 namespace TinyTinaBot
 {
@@ -9,14 +7,6 @@ namespace TinyTinaBot
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .Build();
-
-            BotSettings.Url = config["BotUrl"];
-            BotSettings.Name = config["BotName"];
-            BotSettings.Key = config["BotKey"];
-
             CreateHostBuilder(args).Build().Run();
         }
 
--- TinyTinaBot/Startup.cs
+++ TinyTinaBot/Startup.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using TinyTinaBot.Models;
+using Telegram.Bot;
+using TinyTinaBot.Models.Configurations;
+using TinyTinaBot.Services;
 
 namespace TinyTinaBot
 {
@@ -12,12 +14,19 @@ namespace TinyTinaBot
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            BotConfiguration = Configuration.GetSection("BotConfiguration").Get<BotConfiguration>();
         }
 
         public IConfiguration Configuration { get; }
+        private BotConfiguration BotConfiguration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHostedService<TelegramBotHostedService>();
+            services.AddHttpClient("tgwebhook")
+                .AddTypedClient<ITelegramBotClient>(httpClient 
+                    => new TelegramBotClient(BotConfiguration.BotToken, httpClient));
+            services.AddScoped<IUpdateHandlerService, UpdateHandlerService>();
             services.AddControllers()
                 .AddNewtonsoftJson();
         }
@@ -33,10 +42,11 @@ namespace TinyTinaBot
             app.UseCors();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(name: "tgwebhook",
+                                             pattern: $"bot/{BotConfiguration.BotToken}",
+                                             new { controller = "Webhook", action = "Post" });
                 endpoints.MapControllers();
             });
-
-            Bot.GetBotClientAsync().Wait();
         }
     }
 }
