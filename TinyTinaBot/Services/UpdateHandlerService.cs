4f5d88e886e555356def0d4c6d8f7573b1b9d740
Folkmancer <folkmancer@gmail.com>
2021-10-10 16:01:39 +0300

    Added update handler service.

---
+++ TinyTinaBot/Services/UpdateHandlerService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TinyTinaBot.Services
+{
+    public class UpdateHandlerService
+    {
+        private readonly ILogger<UpdateHandlerService> logger;
+        private readonly ITelegramBotClient botClient;
+
+        public UpdateHandlerService(ILogger<UpdateHandlerService> logger, ITelegramBotClient botClient)
+        {
+            this.logger = logger;
+            this.botClient = botClient;
+        }
+
+        public async Task EchoAsync(Update update)
+        {
+            logger.LogInformation($"Received new update from {update.Message.Chat.Username}");
+            await botClient.SendTextMessageAsync(chatId: update.Message.Chat.Id, text: "Test");
+        }
+    }
+}
